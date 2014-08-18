496fa457e05c1ed7dc20c0566f089b3645a3c8fc
Phildo <Phildo211@gmail.com>
2014-08-18 16:42:36 +0000

    basic window game

---
+++ src/scenes/window/windowScene.js
@@ -0,0 +1,88 @@
+var WindowScene = function(game, canv)
+{
+  var self = this;
+
+  self.ticker;
+  self.clicker;
+  self.drawer;
+  self.assetter;
+
+  self.windows;
+
+  self.numFloors = 5;
+  self.numRooms = 5;
+
+  self.ready = function()
+  {
+    self.ticker = new Ticker();
+    self.clicker = new Clicker();
+    self.drawer = new Drawer(canv);
+    self.assetter = new Assetter();
+
+    self.windows = [];
+
+    for(var i = 0; i < self.numRooms; i++)
+      for(var j = 0; j < self.numFloors; j++)
+        self.windows.push(new WI_Window(self, i, j));
+    for(var i = 0; i < self.windows.length; i++)
+    {
+      self.clicker.register(self.windows[i]);
+      self.drawer.register(self.windows[i]);
+    }
+  };
+
+  self.tick = function()
+  {
+    self.clicker.flush();
+    self.ticker.flush();
+  };
+
+  self.draw = function()
+  {
+    self.drawer.flush();
+  };
+
+  self.cleanup = function()
+  {
+  };
+};
+
+var WI_Window = function(game, room, floor)
+{
+  var self = this;
+
+  self.room = room;
+  self.floor = floor;
+
+  self.x = room*75+100;
+  self.y = floor*75+100;
+  self.w = 50;
+  self.h = 50;
+
+  self.img = game.assetter.asset("assets/man.png");
+
+  var s_CLOSED = "CLOSED";
+  var s_DARKED = "DARKED";
+  var s_OPENED = "OPENED";
+  var states = [s_CLOSED,s_DARKED,s_OPENED];
+
+  self.state = 0;
+
+  self.draw = function(canv)
+  {
+    canv.context.drawImage(self.img,self.x,self.y,self.w,self.h);
+    switch(states[self.state])
+    {
+      case s_CLOSED: canv.context.strokeStyle = "#666666"; break;
+      case s_DARKED: canv.context.strokeStyle = "#000000"; break;
+      case s_OPENED: canv.context.strokeStyle = "#DDDDDD"; break;
+    }
+    canv.context.strokeRect(self.x,self.y,self.w,self.h);
+  }
+
+  self.click = function()
+  {
+    self.state = (self.state+1)%states.length;
+  }
+}
+
