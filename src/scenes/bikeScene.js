f4635d592aa996bdd5bef7af0c9eb8ceabbe1090
Phildo <Phildo211@gmail.com>
2014-12-05 15:42:19 -0600

    bike scene pump tech

--- src/scenes/bikeScene.js
+++ src/scenes/bikeScene.js
@@ -25,7 +25,9 @@ var BikeScene = function(game, stage)
     self.drawer = new Drawer({source:stage.drawCanv});
     self.assetter = new Assetter({});
 
+    self.currentPane = 0;
     self.panes.push(new B_GrabKeysPane(self));
+    self.panes.push(new B_PumpTirePane(self));
 
     /*
     //Examples
@@ -61,7 +63,7 @@ var BikeScene = function(game, stage)
     );
     */
 
-    self.panes[0].begin();
+    self.panes[self.currentPane].begin();
   };
 
   self.tick = function()
@@ -72,12 +74,13 @@ var BikeScene = function(game, stage)
     self.ticker.flush();
 
     var i;
-    if((i = self.panes[0].tick()))
+    if((i = self.panes[self.currentPane].tick()))
     {
-      self.panes[0].end();
+      self.panes[self.currentPane].end();
       if(i == 1) console.log('lose :(');
       if(i == 2) console.log('win :)');
-      self.panes[0].begin();
+      self.currentPane = (self.currentPane+1)%self.panes.length;
+      self.panes[self.currentPane].begin();
     }
   };
 
@@ -85,7 +88,7 @@ var BikeScene = function(game, stage)
   {
     self.drawer.flush();
 
-    self.panes[0].draw();
+    self.panes[self.currentPane].draw();
   };
 
   self.cleanup = function()
@@ -159,6 +162,95 @@ var B_GrabKeysPane = function(scene)
     finished = true;
     won = true;
     console.log('tapped');
+  }
+};
+
+var B_PumpTirePane = function(scene)
+{
+  var finished = false;
+  var won = false;
+
+  var Pump = function(pane)
+  {
+    var self = this;
+    self.x = 200;
+    self.y = 300;
+    self.offY = 0;
+    self.w = 100;
+    self.h = 500;
+
+    self.handleY = 300;
+    self.lastState = 0; //0 = down, 1 = up
+    self.halfCycles = 0;
+
+    self.tick = function()
+    {
+    }
+
+    self.dragStart  = function(evt)
+    {
+      self.offY = self.handleY+(self.h/2)-evt.doY;
+    };
+    self.drag = function(evt)
+    {
+      self.handleY = evt.doY-(self.h/2)+self.offY;
+      if(self.handleY > 380) //handle down
+      {
+
+        if(self.lastState == 1) self.halfCycles++;
+        self.lastState = 0;
+        self.handleY = 380;
+      }
+      if(self.handleY < 220) //handle up
+      {
+        if(self.lastState == 0) self.halfCycles++;
+        self.lastState = 1;
+        self.handleY = 220;
+      }
+    };
+    self.dragFinish = function()
+    {
+    };
+
+    self.draw = function(canv)
+    {
+      canv.context.strokeStyle = "#00FF00";
+      canv.context.strokeRect(self.x,self.y,self.w,self.h);
+      canv.context.strokeRect(self.x,self.handleY,self.w,self.h);
+    }
+  }
+
+  var p;
+  var self = this;
+  self.begin = function()
+  {
+    finished = false;
+    won = false;
+    p = new Pump(self);
+    scene.ticker.register(p);
+    scene.drawer.register(p);
+    scene.dragger.register(p);
+  }
+  self.tick = function() //return 0 for continue, 1 for lose, 2 for win
+  {
+    //let scene handle ticking of doodles, any other ticks can go here
+    if(p.halfCycles > 10)
+    {
+      finished = true;
+      won = true;
+    }
+
+    return finished+won; //#clever
+  }
+  self.draw = function()
+  {
+    //let scene handle drawing of doodles, any other draws can go here
+  }
+  self.end = function()
+  {
+    scene.ticker.unregister(p);
+    scene.drawer.unregister(p);
+    scene.clicker.unregister(p);
   }
 };
 
