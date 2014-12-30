dcdabee6029f1d83ba93755e3a733f9c910064cf
Phildo <Phildo211@gmail.com>
2014-12-30 10:43:10 -0600

    draggable bg on main screen

--- src/scenes/mainScene.js
+++ src/scenes/mainScene.js
@@ -13,6 +13,8 @@ var MainScene = function(game, stage)
   self.particler;
   self.assetter;
 
+  self.map;
+
   self.bikeBtn;
   self.windowBtn;
   self.bulbBtn;
@@ -31,6 +33,7 @@ var MainScene = function(game, stage)
     self.particler = new Particler({});
     self.assetter = new Assetter({});
 
+    self.map         = new MA_Map(self);                             self.drawer.register(self.map);         self.dragger.register(self.map)
     self.bikeBtn     = new MA_Button(20, 20,200,20,"Bike",self);     self.drawer.register(self.bikeBtn);     self.clicker.register(self.bikeBtn);
     self.windowBtn   = new MA_Button(20, 60,200,20,"Window",self);   self.drawer.register(self.windowBtn);   self.clicker.register(self.windowBtn);
     self.bulbBtn     = new MA_Button(20,100,200,20,"Bulb",self);     self.drawer.register(self.bulbBtn);     self.clicker.register(self.bulbBtn);
@@ -44,6 +47,7 @@ var MainScene = function(game, stage)
   {
     self.clicker.flush();
     self.ticker.flush();
+    self.dragger.flush();
   };
 
   self.draw = function()
@@ -81,6 +85,55 @@ var MainScene = function(game, stage)
     if(btn == self.wheelBtn)    game.setScene(WheelScene);
   }
 };
+
+var MA_Map = function(game)
+{
+  var self = this;
+
+  //nice in smooth dragging
+  self.offX = 0;
+  self.offY = 0;
+  self.deltaX = 0;
+  self.deltaY = 0;
+  self.x = 0;
+  self.y = 0;
+  self.w = game.stage.drawCanv.canvas.width*4;
+  self.h = game.stage.drawCanv.canvas.height*2;
+
+  var boundx = 0;
+  var boundy = 0;
+  var boundw = game.stage.drawCanv.canvas.width;
+  var boundh = game.stage.drawCanv.canvas.height;
+
+  self.dragStart = function(evt)
+  {
+    self.offX = self.x+(self.w/2)-evt.doX;
+    self.offY = self.y+(self.h/2)-evt.doY;
+  };
+  self.drag = function(evt)
+  {
+    self.deltaX = (evt.doX-(self.w/2)+self.offX)-self.x;
+    self.deltaY = (evt.doY-(self.h/2)+self.offY)-self.y;
+    if(self.x + self.deltaX > boundx)                 self.deltaX = boundx-self.x;
+    if(self.x + self.deltaX < boundx-(self.w-boundw)) self.deltaX = boundx-(self.w-boundw)-self.x;
+    if(self.y + self.deltaY > boundy)                 self.deltaY = boundy-self.y;
+    if(self.y + self.deltaY < boundy-(self.h-boundh)) self.deltaY = boundy-(self.h-boundh)-self.y;
+    self.x += self.deltaX;
+    self.y += self.deltaY;
+  };
+  self.dragFinish = function()
+  {
+  };
+  self.draw = function(canv)
+  {
+    canv.context.strokeStyle = "#000000";
+    canv.context.lineWidth = 1;
+    for(var i = 0; i < 100; i+=2)
+      canv.context.strokeRect(self.x+(self.w/100)*i, self.y, self.w/100, self.h);
+    for(var i = 0; i < 100; i+=2)
+      canv.context.strokeRect(self.x, self.y+(self.h/100)*i, self.w, self.h/100);
+  }
+}
 
 var MA_Button = function(x,y,w,h,text,game)
 {
